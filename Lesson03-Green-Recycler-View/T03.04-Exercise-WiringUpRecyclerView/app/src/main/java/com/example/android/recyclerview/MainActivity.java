1f88e8b83db93ae1a6fa89d6bcd51cdc3bb60a8d
rray89 <rray@hotmail.ca>
2018-01-04 20:26:05 -0400

    finished T03.04

--- Lesson03-Green-Recycler-View/T03.04-Exercise-WiringUpRecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
+++ Lesson03-Green-Recycler-View/T03.04-Exercise-WiringUpRecyclerView/app/src/main/java/com/example/android/recyclerview/MainActivity.java
@@ -17,28 +17,57 @@ package com.example.android.recyclerview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a private static final int called NUM_LIST_ITEMS and set it equal to 100
+    // COMPLETED TODO (1) Create a private static final int called NUM_LIST_ITEMS and set it equal to 100
+    private static final int NUM_LIST_ITEMS = 100;
 
-    // TODO (2) Create a GreenAdapter variable called mAdapter
-    // TODO (3) Create a RecyclerView variable called mNumbersList
+    // COMPLETED TODO (2) Create a GreenAdapter variable called mAdapter
+    /*
+     * References to RecyclerView and Adapter to reset the list to its
+     * "pretty" state when the reset menu item is clicked.
+     */
+    private GreenAdapter mAdapter;
+
+    // COMPLETED TODO (3) Create a RecyclerView variable called mNumbersList
+    private RecyclerView mNumbersList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // TODO (4) Use findViewById to store a reference to the RecyclerView in mNumbersList
+        // COMPLETED TODO (4) Use findViewById to store a reference to the RecyclerView in mNumbersList
+        mNumbersList = findViewById(R.id.rv_numbers);
 
-        // TODO (5) Create a LinearLayoutManager variable called layoutManager
-        // TODO (6) Use setLayoutManager on mNumbersList with the LinearLayoutManager we created above
+        // COMPLETED TODO (5) Create a LinearLayoutManager variable called layoutManager
+        /*
+         * A LinearLayoutManager is responsible for measuring and positioning item views within a
+         * RecyclerView into a linear list. This means that it can produce either a horizontal or
+         * vertical list depending on which parameter you pass in to the LinearLayoutManager
+         * constructor. By default, if you don't specify an orientation, you get a vertical list.
+         * In our case, we want a vertical list, so we don't need to pass in an orientation flag to
+         * the LinearLayoutManager constructor.
+         *
+         * There are other LayoutManagers available to display your data in uniform grids,
+         * staggered grids, and more! See the developer documentation for more details.
+         */
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
 
-        // TODO (7) Use setHasFixedSize(true) to designate that the contents of the RecyclerView won't change an item's size
+        // COMPLETED TODO (6) Use setLayoutManager on mNumbersList with the LinearLayoutManager we created above
+        mNumbersList.setLayoutManager(layoutManager);
 
-        // TODO (8) Store a new GreenAdapter in mAdapter and pass it NUM_LIST_ITEMS
+        // COMPLETED TODO (7) Use setHasFixedSize(true) to designate that the contents of the RecyclerView won't change an item's size
+        mNumbersList.setHasFixedSize(true);
 
-        // TODO (9) Set the GreenAdapter you created on mNumbersList
+        // COMPLETED TODO (8) Store a new GreenAdapter in mAdapter and pass it NUM_LIST_ITEMS
+        /* The GreenAdapter is responsible for displaying each item in the list. */
+        mAdapter = new GreenAdapter(NUM_LIST_ITEMS);
+
+        // COMPLETED TODO (9) Set the GreenAdapter you created on mNumbersList
+        mNumbersList.setAdapter(mAdapter);
     }
 }
