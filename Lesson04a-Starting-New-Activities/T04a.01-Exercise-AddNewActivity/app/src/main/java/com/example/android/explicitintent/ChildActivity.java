2b03e719b34dafa30c099d21b3dd43ccbef2e7a7
rray89 <rray@hotmail.ca>
2018-01-30 15:27:07 -0400

    finished T04a.01

---
+++ Lesson04a-Starting-New-Activities/T04a.01-Exercise-AddNewActivity/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -0,0 +1,20 @@
+package com.example.android.explicitintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ChildActivity extends AppCompatActivity {
+
+    // COMPLETED TODO (6) Create a TextView field to display your message
+    private TextView mDisplayText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+
+        // COMPLETED TODO (7) Get a reference to your TextView in Java
+        mDisplayText = (TextView) findViewById(R.id.tv_display);
+    }
+}
